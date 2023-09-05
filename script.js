ed83d66b911f183371971c41bbe2b8f42e8eacab
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-05 14:40:28 +0200

    Random review functionality add

--- script.js
+++ script.js
@@ -24,9 +24,18 @@ let reviews = [
     review:
       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
+  {
+    id: "4",
+    img: "./images/person-4.jpg",
+    name: "bill anderson",
+    profession: "the boss",
+    review:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
+  },
 ];
 
 let count = 0;
+let random = Math.floor(Math.random() * 4);
 
 let img = document.getElementById("img");
 let authorName = document.getElementById("author-name");
@@ -36,9 +45,21 @@ let review = document.getElementById("review");
 window.addEventListener("load", showNextReview);
 
 let nextBtn = document.getElementById("next-btn");
-nextBtn.addEventListener("click", () => (count < 2 ? ++count : (count = 0)));
+nextBtn.addEventListener("click", () => (count < 3 ? ++count : (count = 0)));
 nextBtn.addEventListener("click", showNextReview);
 
+let previousBtn = document.getElementById("previous-btn");
+previousBtn.addEventListener("click", () =>
+  count == 0 ? console.log((count = 3)) : console.log(--count)
+);
+previousBtn.addEventListener("click", showNextReview);
+
+let surpriseBtn = document.getElementById("btn-surprise-me");
+surpriseBtn.addEventListener(
+  "click",
+  () => (count = Math.floor(Math.random() * 4))
+);
+surpriseBtn.addEventListener("click", showNextReview);
 function showNextReview() {
   img.src = reviews[count].img;
   authorName.innerText = reviews[count].name;
